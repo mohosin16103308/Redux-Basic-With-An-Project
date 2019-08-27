96f4a29e2cf68db4de664046eb15276c20ef808f
mohosin16103308 <hamza1610330816@gmail.com>
2019-08-27 09:05:00 +0600

    Dispatching Actions

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Redux Basic With an Project
+
+1.npm install    for install node module 
+
+2. npm install --save redux  for install redux 
+
+3. Run redux-basic.js file using CMD : node redux-basic.js
--- redux-basic.js
+++ redux-basic.js
@@ -1,14 +1,55 @@
+
 const redux = require('redux');
 const createStore = redux.createStore;
 
+// initial state 
+const initialSTate = {
+    counter : 0,
+
+}
+
 //Reducer
-const rootReducer = (state,action) => {
+const rootReducer = (state=initialSTate,action) => {
+
+    //Handle Dispatching Action INC_COUNTER
+
+    if(action.type === 'INC_COUNTER'){
+        return{
+            ...state,
+            counter:state.counter + 1
+        }
+    } 
+    
+    
+    //Handle Dispatching Action ADD_COUNTER
+
+    if(action.type === 'ADD_COUNTER'){
+        return{
+            ...state,
+            counter:state.counter + action.value
+        }
+    }
+
+
   return state;
 }
+
+
 //Store
 const store = createStore(rootReducer);
+
 console.log(store.getState());
 
 //Dispatching ACtion
+
+
+store.dispatch({type:'INC_COUNTER'});
+store.dispatch({type:'INC_COUNTER'});
+store.dispatch({type:'ADD_COUNTER',value:10});
+
+
+
+console.log(store.getState());
+
 
 //Subscription
